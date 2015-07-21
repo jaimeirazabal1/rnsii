2d05b1d28cb00d765a7fe6a4435bf1a83e1bef77
jaimeirazabal1 <jaimeirazabal1@gmail.com>
2015-07-20 20:38:45 -0430

    quitando la clave de nuevo

--- rnsii/config/db.php
+++ rnsii/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rnsii',
     'username' => 'root',
-    'password' => '16923509j',
+    'password' => '',
     'charset' => 'utf8',
 ];
